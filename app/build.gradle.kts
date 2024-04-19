9f05438f72d6ef781a24b2f9b7a8fb47ace3df67
sumitrai <developer2607@gmail.com>
2024-04-19 22:26:41 +0530

    project created and  dependency added for http and room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,24 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //project dependecy required
+    //for api call retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.8.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    //for image loading
+    implementation("com.squareup.picasso:picasso:2.8")
+    // Room
+    val roomVersion = "2.5.2"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation("androidx.compose.runtime:runtime-livedata:1.4.3")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+        //for lottie animation
+    implementation("com.airbnb.android:lottie-compose:6.0.1")
+    //for dynamic sdp and dp
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+
 }
