e9ada32806fe077a15e983987f4dff0777445c98
sumitrai <developer2607@gmail.com>
2024-04-20 16:07:39 +0530

    room db implemented

--- app/src/main/java/com/example/movieappwithmvvm/extra/Constants.kt
+++ app/src/main/java/com/example/movieappwithmvvm/extra/Constants.kt
@@ -1,9 +1,10 @@
 package com.example.movieappwithmvvm.extra
 
 object Constants {
-    const val URL = "https://api.themoviedb.org/3/movie/popular?api_key=2147294b386ecadcb05cd19bcbbdddb9&page=1"
+
+    const val URL = "https://api.themoviedb.org/3/movie/popular?api_key=3aa594abde32277be6ee3c7c41f16141&page=1"
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY = "2147294b386ecadcb05cd19bcbbdddb9"
+    const val API_KEY = "3aa594abde32277be6ee3c7c41f16141"
     const val TIMEOUT_TIME: Long = 60
     const val MOVIE_URL = "https://image.tmdb.org/t/p/w500/"
     const val START_PAGE = 1
